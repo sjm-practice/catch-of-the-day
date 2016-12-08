101965b3704be67002dfbbe7bd1a765a452231de
stevenjmarsh <sjm.atl@gmail.com>
2016-12-07 19:46:13 -0500

    adding helpers (for fun name generation)

--- imports/ui/components/StorePicker.jsx
+++ imports/ui/components/StorePicker.jsx
@@ -1,6 +1,7 @@
 import React, {
   Component,
 } from "react";
+import { getFunName } from "../../utils/helpers";
 
 class StorePicker extends Component {
   render() {
@@ -8,7 +9,7 @@ class StorePicker extends Component {
       <form className="store-selector">
         { /* a comment in JSX - must be inside a parent element */ }
         <h2>Please Enter A Store</h2>
-        <input type="text" placeholder="Store Name" />
+        <input type="text" placeholder="Store Name" defaultValue={getFunName()} />
         <button type="submit">Visit Store</button>
       </form>
     );
