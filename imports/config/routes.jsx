cf9839b3f7c1ed4e96efad1fb0a0c43345ca057c
stevenjmarsh <sjm.atl@gmail.com>
2016-12-07 19:33:40 -0500

    adding a not found route and page

--- imports/config/routes.jsx
+++ imports/config/routes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Match, Miss } from "react-router";
 import App from "../ui/components/App";
 import StorePicker from "../ui/components/StorePicker";
+import NotFound from "../ui/components/NotFound";
 
 const routes = (
   <BrowserRouter>
     <div>
       <Match exactly pattern="/" component={StorePicker} />
       <Match pattern="/store/:storeId" component={App} />
+      <Miss component={NotFound} />
     </div>
   </BrowserRouter>
 );
