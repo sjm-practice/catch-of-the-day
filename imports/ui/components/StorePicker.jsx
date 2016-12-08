afbb1677c637af2412a3635941846a353e01df4d
stevenjmarsh <sjm.atl@gmail.com>
2016-12-07 19:57:38 -0500

    adding initial submit event to StorePicker

--- imports/ui/components/StorePicker.jsx
+++ imports/ui/components/StorePicker.jsx
@@ -4,9 +4,14 @@ import React, {
 import { getFunName } from "../../utils/helpers";
 
 class StorePicker extends Component {
+  goToStore(event) {
+    event.preventDefault();
+    console.log("submitted!");
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         { /* a comment in JSX - must be inside a parent element */ }
         <h2>Please Enter A Store</h2>
         <input type="text" placeholder="Store Name" defaultValue={getFunName()} />
