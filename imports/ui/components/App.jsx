1c0abb72012b1cbeaae66b347f1d8e1bf391c5ee
stevenjmarsh <sjm.atl@gmail.com>
2016-12-08 17:56:39 -0500

    adding addFish method

--- imports/ui/components/App.jsx
+++ imports/ui/components/App.jsx
@@ -12,6 +12,19 @@ class App extends Component {
       fishes: {},
       order: {},
     };
+
+    this.addFish = this.addFish.bind(this);
+  }
+
+  addFish(fish) {
+    // save current state (current list of fishes)
+    const fishes = { ...this.state.fishes };
+    // get timestamp to create a unique key for the new fish
+    const timestamp = Date.now();
+    // add the new fish to the fishes object (list of fishes)
+    fishes[`fish-${timestamp}`] = fish;
+    // set the new fishes state
+    this.setState({ fishes });
   }
 
   render() {
