4e43d64cec1983caf3798809423081a41d55f7bd
stevenjmarsh <sjm.atl@gmail.com>
2016-12-08 17:18:47 -0500

    adding capturing field from form in to an object

--- imports/ui/components/AddFishForm.jsx
+++ imports/ui/components/AddFishForm.jsx
@@ -3,20 +3,27 @@ import React, { Component } from "react";
 class AddFishForm extends Component {
   createFish(event) {
     event.preventDefault();
-    console.log("creating a fish!");
+    const fish = {
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value,
+    };
+    console.log("creating a fish:", fish);
   }
 
   render() {
     return (
       <form className="fish-edit" onSubmit={e => this.createFish(e)}>
-        <input type="text" placeholder="Fish Name"/>
-        <input type="text" placeholder="Fish Price"/>
-        <select>
+        <input ref={(input) => { this.name = input; }} type="text" placeholder="Fish Name" />
+        <input ref={(input) => { this.price = input; }} type="text" placeholder="Fish Price" />
+        <select ref={(input) => { this.status = input; }} >
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea placeholder="Fish Desc"/>
-        <input type="text" placeholder="Fish Image"/>
+        <textarea ref={(input) => { this.desc = input; }} placeholder="Fish Desc" />
+        <input ref={(input) => { this.image = input; }} type="text" placeholder="Fish Image" />
         <button type="submit">+ Add Item</button>
       </form>
     );
