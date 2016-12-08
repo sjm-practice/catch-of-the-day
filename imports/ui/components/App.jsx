d93ad86bf6e921de8e5f77e0f7f839bcd7f44b3d
stevenjmarsh <sjm.atl@gmail.com>
2016-12-08 17:31:42 -0500

    adding initial state for app  fishes, order

--- imports/ui/components/App.jsx
+++ imports/ui/components/App.jsx
@@ -6,11 +6,19 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fishes: {},
+      order: {},
+    };
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market"/>
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
         <Inventory />
