3b8d076b19df6953bcbbdfdb3ff7dee61ae96a52
stevenjmarsh <sjm.atl@gmail.com>
2016-12-08 17:05:43 -0500

    adding initial form event handler for submit

--- imports/ui/components/AddFishForm.jsx
+++ imports/ui/components/AddFishForm.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 
 class AddFishForm extends Component {
+  createFish(event) {
+    event.preventDefault();
+    console.log("creating a fish!");
+  }
+
   render() {
     return (
-      <form className="fish-edit">
-        <input type="text" placeholder="Fish Name" />
-        <input type="text" placeholder="Fish Price" />
+      <form className="fish-edit" onSubmit={e => this.createFish(e)}>
+        <input type="text" placeholder="Fish Name"/>
+        <input type="text" placeholder="Fish Price"/>
         <select>
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea placeholder="Fish Desc" />
-        <input type="text" placeholder="Fish Image" />
+        <textarea placeholder="Fish Desc"/>
+        <input type="text" placeholder="Fish Image"/>
         <button type="submit">+ Add Item</button>
       </form>
     );
