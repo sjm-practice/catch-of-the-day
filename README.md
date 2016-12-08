d125a78992b32fcca048285333f93edcf2b10195
stevenjmarsh <sjm.atl@gmail.com>
2016-12-08 10:36:28 -0500

    moving submit form handler's binding of this from property arrow function to constructor

--- README.md
+++ README.md
@@ -11,5 +11,9 @@ Coding along with React For Beginners tutorial, however using Meteor and adding 
 * start with Meteor React Testing Boilerplate
 * had to add assets (css, fonts, images) from tutorial repo, to public folder
 * need to install specific version of react router (v4 alpha)
-* one step in the tutorial saves a reference to a an input component value using ref with a function
+* one step in the tutorial saves a reference to an input component value using ref with a function
+    * instead of using an id string for a reference (aks string ref), a function is used (aka functin ref), that is executed when the component is rendered
+        * this provides a reference to the whole component, and you can access the value from that
     * an explanation of that can be found [here](https://facebook.github.io/react/docs/refs-and-the-dom.html)
+* when and why binding 'this' to methods is done, good explanation found [here](https://facebook.github.io/react/docs/handling-events.html)
+    * ie, in component constructors you may see `this.handleClick = this.handleClick.bind(this)`
--- imports/ui/components/StorePicker.jsx
+++ imports/ui/components/StorePicker.jsx
@@ -4,14 +4,21 @@ import React, {
 import { getFunName } from "../../utils/helpers";
 
 class StorePicker extends Component {
+  constructor() {
+    super();
+    // binding this to the method, so 'this' will be available in callback
+    //   for more info, see bidning note in readme
+    this.goToStore = this.goToStore.bind(this);
+  }
+
   goToStore(event) {
     event.preventDefault();
-    console.log("input:", this.storeInput.value);
+    console.log("input:", this.storePickerInput.value);
   }
 
   render() {
     return (
-      <form className="store-selector" onSubmit={e => this.goToStore(e)}>
+      <form className="store-selector" onSubmit={this.goToStore}>
         { /* a comment in JSX - must be inside a parent element */ }
         <h2>Please Enter A Store</h2>
         { /* see readme for explanation of using ref with a function */ }
@@ -19,7 +26,7 @@ class StorePicker extends Component {
           type="text"
           placeholder="Store Name"
           defaultValue={getFunName()}
-          ref={(input) => { this.storeInput = input; }}
+          ref={(input) => { this.storePickerInput = input; }}
         />
         <button type="submit">Visit Store</button>
       </form>
