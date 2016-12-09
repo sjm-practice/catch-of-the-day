3a268378bad4a8ccc9db8b4d3b11bf9f33418f14
stevenjmarsh <sjm.atl@gmail.com>
2016-12-09 12:17:25 -0500

    reverting order stateless function, back to a component (needed use of this and methods)

--- imports/ui/components/Order.jsx
+++ imports/ui/components/Order.jsx
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import { formatPrice } from "../../utils/helpers";
 
-const Order = (props) => {
-  const orderIds = Object.keys(props.order);
+class Order extends Component {
+  constructor() {
+    super();
+    this.renderOrderItem = this.renderOrderItem.bind(this);
+  }
 
-  const total = orderIds.reduce((prevTotal, key) => {
-    const fish = props.fishes[key];
-    if (fish && fish.status === "available") {
-      const count = props.order[key];
-                        // this protects against fish being made unavailable during calculation
-      return prevTotal + (count * fish.price || 0);
-    }
+  renderOrderItem(key) {
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
 
-    return prevTotal;
-  }, 0);
+    return (
+      <li key={key}>{count} lbs. {fish.name}</li>
+    );
+  }
 
-  return (
-    <div className="order-wrap">
-      <h2>Your Order</h2>
-      <ul className="order">
-        <li className="total">
-          <strong>Total:</strong>
-          {formatPrice(total)}
-        </li>
-      </ul>
-    </div>
-  );
-};
+  render() {
+    const orderIds = Object.keys(this.props.order);
+
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      if (fish && fish.status === "available") {
+        const count = this.props.order[key];
+        // this protects against fish being made unavailable during calculation
+        return prevTotal + (count * fish.price || 0);
+      }
+
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+        <h2>Your Order</h2>
+        <ul className="order">
+          { orderIds.map(this.renderOrderItem) }
+          <li className="total">
+            <strong>Total:</strong>
+            {formatPrice(total)}
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Order;
