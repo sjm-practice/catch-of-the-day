6fc74eaf6f94049512d980caa2095c657ae062f1
stevenjmarsh <sjm.atl@gmail.com>
2016-12-09 12:30:01 -0500

    formatting order item

--- imports/ui/components/Order.jsx
+++ imports/ui/components/Order.jsx
@@ -11,8 +11,15 @@ class Order extends Component {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
 
+    if (!fish || fish.status === "unavailable") {
+      return <li key={key}>Sorry, {fish ? fish.name : "fish"} is no longer available.</li>;
+    }
+
     return (
-      <li key={key}>{count} lbs. {fish.name}</li>
+      <li key={key}>
+        <span>{count} {count > 1 ? "lbs" : "lb"} {fish.name}</span>
+        <span className="price">{formatPrice(count * fish.price)}</span>
+      </li>
     );
   }
 
