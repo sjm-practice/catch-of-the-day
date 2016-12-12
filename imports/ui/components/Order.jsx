636225aef6262b44231f4e7cf53e1053ab312b84
stevenjmarsh <sjm.atl@gmail.com>
2016-12-12 12:23:39 -0500

    adding animating adding fish to order (slide in from left)

--- imports/ui/components/Order.jsx
+++ imports/ui/components/Order.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { formatPrice } from "../../utils/helpers";
+import CSSTransitionGroup from "react-addons-css-transition-group";
 
 class Order extends Component {
   constructor() {
@@ -55,13 +56,19 @@ class Order extends Component {
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
-        <ul className="order">
+        <CSSTransitionGroup
+          className="order"
+          component="ul"
+          transitionName="order"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
           { orderIds.map(this.renderOrderItem) }
           <li className="total">
             <strong>Total:</strong>
             {formatPrice(total)}
           </li>
-        </ul>
+        </CSSTransitionGroup>
       </div>
     );
   }
